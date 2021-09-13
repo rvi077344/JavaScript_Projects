e2f7c80708d5ac9a2f374ddc0086a3d9716f574c
Ravi Ranjan Singh <56018516+rvi077344@users.noreply.github.com>
2021-09-13 10:33:44 +0530

    Add files via upload

---
+++ Increament Counter/main.js
@@ -0,0 +1,25 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerHTML = '0' ;
+
+    const updateCounter =  () => {
+
+        const target = Number(counter.getAttribute('data-target'));
+        const c = Number(counter.innerHTML) ;
+
+        const increment = target / 1000 ;
+
+        if(c<target)
+        {
+            counter.innerHTML = `${Math.ceil( c + increment)}`
+            setTimeout(updateCounter ,1)
+
+        }
+        else{
+            counter.innerHTML = target ;
+        }
+
+    }
+    updateCounter();
+})
